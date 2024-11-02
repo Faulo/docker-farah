b9ddbec02f3668478366d77d90aa78c3b8c33553
Daniel Schulz <faulolio@gmail.com>
2024-11-02 15:21:48 +0100

    fix: no host

--- docker-build.bat
+++ docker-build.bat
@@ -1,4 +1,4 @@
 SET PHP_VERSION=8.0
 for /f %%i in ('docker info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-call docker build --network=host -t faulo/farah:test --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+call docker build -t faulo/farah:test --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
 pause
