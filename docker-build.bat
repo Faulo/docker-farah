c955527f18c568be81d0332d15adbb46b3b950f1
Daniel Schulz <faulolio@gmail.com>
2024-12-16 02:45:08 +0100

    fix: PHP

--- docker-build.bat
+++ docker-build.bat
@@ -1,4 +1,4 @@
-SET PHP_VERSION=8.0
+SET PHP_VERSION=8.3
 for /f %%i in ('docker info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-call docker build -t faulo/farah:test --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+call docker build --isolation=hyperv -t faulo/farah:test --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
 pause
