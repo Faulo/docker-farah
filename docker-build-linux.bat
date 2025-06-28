dae15021dce2dbb5e2ee974aa205686112c1e7f6
Daniel Schulz <faulolio@gmail.com>
2025-06-28 20:36:33 +0200

    feat: more php.ini settings, imagick

---
+++ docker-build-linux.bat
@@ -0,0 +1,7 @@
+setlocal
+cd %~dp0
+call load-env
+SET DOCKER_OS=linux
+call docker --context groke build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+endlocal
+pause
