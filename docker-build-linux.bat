4975a1aeec411fec2cca1308f73a0c7f4a8206ad
Daniel Schulz <faulolio@gmail.com>
2025-06-28 21:10:31 +0200

    devops: .bat

--- docker-build-linux.bat
+++ docker-build-linux.bat
@@ -4,4 +4,4 @@ call load-env
 SET DOCKER_OS=linux
 call docker --context groke build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
 endlocal
-pause
+rem pause
--- docker-build.bat
+++ docker-build.bat
@@ -2,6 +2,6 @@ setlocal
 cd %~dp0
 call load-env
 for /f %%i in ('docker info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-call docker build --isolation=hyperv -t faulo/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+call docker build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
 endlocal
 pause
