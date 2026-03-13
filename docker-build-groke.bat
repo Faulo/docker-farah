1218f9698f99062496f0cdb110cf3ea4ddfb8520
Daniel Schulz <faulolio@gmail.com>
2026-03-13 02:52:11 +0100

    feat: support for php 8.5

--- docker-build-groke.bat
+++ docker-build-groke.bat
@@ -2,6 +2,6 @@ setlocal
 cd %~dp0
 call load-env
 for /f %%i in ('docker --context groke info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-call docker --context groke build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+call docker --context groke build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% --build-arg DEBIAN=%DEBIAN% %DOCKER_OS%
 endlocal
 rem pause
