db5113dcbd5b8e9070c5e837492c61faf0287876
Daniel Schulz <faulolio@gmail.com>
2026-04-20 18:46:28 +0200

    devops: min-version 8.2

--- docker-build.bat
+++ docker-build.bat
@@ -2,6 +2,6 @@ setlocal
 cd %~dp0
 call load-env
 for /f %%i in ('docker info --format "{{.OSType}}"') do SET DOCKER_OS=%%i
-call docker build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% %DOCKER_OS%
+call docker build -t tmp/farah:%PHP_VERSION% --build-arg PHP_VERSION=%PHP_VERSION% --build-arg DEBIAN=%DEBIAN% %DOCKER_OS%
 endlocal
 rem pause
